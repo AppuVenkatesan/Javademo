881bd83dcc36c2a16bc9c90c316bbf54bc4b7f41
appu <appu.v222@gmail.com>
2024-10-18 20:59:18 +0530

    this 1st user commit

---
+++ src/demopack/Constructordemo.java
@@ -0,0 +1,28 @@
+package demopack;
+
+class demo{
+	
+	 int num;
+	 
+	 
+	 
+	 public demo() {
+		 
+		 num=7;
+	 }
+}
+
+public class Constructordemo {
+	
+	
+
+
+	public static void main(String[] args) {
+		
+		demo obj = new demo();
+		System.out.println(obj.num);
+      	
+		
+	}
+
+}
